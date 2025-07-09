0b4a67021d689b2424faac9a41212770d20e7257
Petros <petrosluros@gmail.com>
2025-07-09 20:54:49 +0300

    teacher/region repo

--- src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
@@ -2,12 +2,19 @@ package gr.aueb.cf.schoolapp.repository;
 
 import gr.aueb.cf.schoolapp.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher,Long> {
 
-    List<Teacher> findByRegion()
+    List<Teacher> findByRegion(Long id);
+    Optional<Teacher> findByUUID(String uuid);
+    Optional<Teacher> findByVat(String vat);
+
+    @Query("SELECT count(t) FROM Teacher t WHERE t.uuid = ?!")
+    long getCount(String uuid);
 }
