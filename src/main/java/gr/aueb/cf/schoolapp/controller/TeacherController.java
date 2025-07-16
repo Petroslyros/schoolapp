ae1a334f8cdf04e66246efc79e5653f574fe8368
Petros <petrosluros@gmail.com>
2025-07-16 19:14:07 +0300

    Service, TeacherInsertValidator

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -3,9 +3,12 @@ package gr.aueb.cf.schoolapp.controller;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.repository.RegionRepository;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,49 +18,56 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/school/teachers")
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class TeacherController {
 
     private final ITeacherService teacherService;
     private final RegionRepository regionRepository;
+    private final Mapper mapper;
+    private final TeacherInsertValidator teacherInsertValidator;
 
-    @Autowired
-    public TeacherController(ITeacherService teacherService, RegionRepository regionRepository) {
-        this.teacherService = teacherService;
-        this.regionRepository = regionRepository;
-    }
+
+
+//    @Autowired
+//    public TeacherController(ITeacherService teacherService, RegionRepository regionRepository, Mapper mapper) {
+//        this.teacherService = teacherService;
+//        this.regionRepository = regionRepository;
+//        this.mapper = mapper;
+//    }
 
     @GetMapping("/insert")
-    public String getTeacherForm(Model model){
-     model.addAttribute("teacherInsertDTO", new TeacherInsertDTO());
-     model.addAttribute("regions", regionRepository.findAll());
-
-     return "teacher-form";
+    public String getTeacherForm(Model model) {
+        model.addAttribute("teacherInsertDTO", new TeacherInsertDTO());
+        model.addAttribute("regions", regionRepository.findAll(Sort.by("name"))); // PagingAndSortingRepository
+        return "teacher-form";
     }
 
-    public String saveTeacher(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO ,
+    @PostMapping("/insert")
+    public String saveTeacher(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
                               BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
-
         Teacher savedTeacher;
-        if(bindingResult.hasErrors()) {
+
+        teacherInsertValidator.validate(teacherInsertDTO,bindingResult);
+
+        if (bindingResult.hasErrors()) {
             model.addAttribute("regions", regionRepository.findAll(Sort.by("name")));
             return "teacher-form";
         }
 
         try {
             savedTeacher = teacherService.saveTeacher(teacherInsertDTO);
-
-
-
+            TeacherReadOnlyDTO readOnlyDTO = mapper.mapToTeacherReadOnlyDTO(savedTeacher);
+            redirectAttributes.addFlashAttribute("teacher", readOnlyDTO);
+            return "redirect:/school/teachers";
         } catch (EntityAlreadyExistsException | EntityInvalidArgumentException e) {
             model.addAttribute("regions", regionRepository.findAll(Sort.by("name")));
             model.addAttribute("errorMessage", e.getMessage());
-
             return "teacher-form";
         }
     }
--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -3,8 +3,13 @@ package gr.aueb.cf.schoolapp.service;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import org.springframework.data.domain.Page;
 
 public interface ITeacherService {
     Teacher saveTeacher(TeacherInsertDTO teacherInsertDTO) throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    //page is a generic class that contains info about the paginated page
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(int page, int size);
 }
