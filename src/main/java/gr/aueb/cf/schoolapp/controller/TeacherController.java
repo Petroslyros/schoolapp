0b50b283ce2d56884d75b61ba257991cc5645863
Petros <petrosluros@gmail.com>
2025-07-16 19:44:52 +0300

    Service, TeacherInsertValidator

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -12,14 +12,13 @@ import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -64,11 +63,25 @@ public class TeacherController {
             savedTeacher = teacherService.saveTeacher(teacherInsertDTO);
             TeacherReadOnlyDTO readOnlyDTO = mapper.mapToTeacherReadOnlyDTO(savedTeacher);
             redirectAttributes.addFlashAttribute("teacher", readOnlyDTO);
-            return "redirect:/school/teachers";
+            return "redirect:/school/teachers/view";
         } catch (EntityAlreadyExistsException | EntityInvalidArgumentException e) {
             model.addAttribute("regions", regionRepository.findAll(Sort.by("name")));
             model.addAttribute("errorMessage", e.getMessage());
             return "teacher-form";
         }
+    }
+
+    @GetMapping("/view")
+    public String getPaginatedTeacher(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            Model model) {
+        Page<TeacherReadOnlyDTO> teachersPage = teacherService.getPaginatedTeachers(page,size);
+
+        model.addAttribute("teachersPage", teachersPage);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", teachersPage.getTotalPages());
+
+        return "teachers";
     }
 }
